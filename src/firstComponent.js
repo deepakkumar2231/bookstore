e8cc7b75813b346f8ca238dab2747a61516892ef
adminsopra <deepak.kumar3@soprasteria.com>
2021-08-10 10:01:04 +0530

    first component  commit in react app

---
+++ src/firstComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+
+
+function FirstComponent() {
+    return (
+        <React.Fragment>
+            <div>
+                <h3>
+                    This is my first component
+                </h3>
+            </div>
+        </React.Fragment>
+    )
+}
+export default FirstComponent
