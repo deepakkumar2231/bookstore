d053dfcfce616fc8e3f97289ff76113fd23deda9
adminsopra <deepak.kumar3@soprasteria.com>
2021-08-10 10:56:03 +0530

    second component  commit in react app

---
+++ src/secondComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "react-dom";
+
+
+function SecondComponent() {
+    return (
+        <React.Fragment>
+            <div>
+                <h3>
+                    This is my second component
+                </h3>
+                <img src="motoki-tonn-vV1a1Leq-dQ-unsplash.jpg" alt="this is second image" width="500" height="500"></img>
+            </div>
+        </React.Fragment>
+    )
+}
+export default SecondComponent
