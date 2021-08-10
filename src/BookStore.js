796a4185507ac5cf16a1c1c8386fb03369398602
adminsopra <deepak.kumar3@soprasteria.com>
2021-08-10 16:00:04 +0530

    Nested  commit in react app

--- src/BookStore.js
+++ src/BookStore.js
@@ -1,24 +1,39 @@
 import React from'react'
 import './index.css'
+const FirstCar={
+img:"stefan-rodriguez-2AovfzYV3rc-unsplash.jpg",
+brand:'Cheverolet',
+ speed:"300"
+}
+   const SecondCar={
+       img:'dhiva-krishna-YApS6TjKJ9c-unsplash.jpg',
+       brand:'mercedez',
+       speed:"250"
+   
+}
+const ThirdCar={
+    img:"viktor-theo-aIDkOU7eGgo-unsplash.jpg",
+    brand:'buggati',
+    speed:"450"
+}
 function BookStore()
 {
     return(
         <section className="booklist">
-            <Book />
-            <Book />
-            <Book />
+            <Book img={FirstCar.img} brand={FirstCar.brand} speed={FirstCar.speed} />
+            <Book img={SecondCar.img} brand={SecondCar.brand} speed={SecondCar.speed}></Book>
+            <Book img={ThirdCar.img} brand={ThirdCar.brand} speed={ThirdCar.speed}></Book>
         </section>
     )
 }
-const Book=()=>{
-    return <article>
-        <Img></Img>
-        <Company></Company>
-        <Speed></Speed>
+const Book=({img,brand,speed})=>{
+    return <article className='Book'>
+       <img src={img} height='250' width='500'></img>
+       <h1>{brand}</h1>
+       <h2>{speed}</h2>
     </article>
 }
-const Img=()=><img src="stefan-rodriguez-2AovfzYV3rc-unsplash.jpg" alt="this is car image" width="500" height="500"></img>
-const Company=()=><h1 style={{color:'#32a852',fontsize:'0.75rem',marginTop:"0.25rem"}}>Cheverolet</h1>
 
-const Speed=()=><h2 style={{color:'#a632a8',fontsize:'0.75rem',marginTop:"0.25rem"}}>300</h2>
+
+const Speed=()=><h2 style={{color:'#a632a8',fontsize:'0.75rem',marginTop:"0.25rem"}}>speed</h2>
 export default BookStore
